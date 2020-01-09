e954c8acb4204726782aeb16c7648dcbf2c310be
hughzc <hughzcheng@163.com>
2020-01-09 15:04:41 +0800

    20200109 mybatis

--- src/main/java/huzc/community/mycommunity/controller/AuthorizeController.java
+++ src/main/java/huzc/community/mycommunity/controller/AuthorizeController.java
@@ -2,14 +2,18 @@ package huzc.community.mycommunity.controller;
 
 import huzc.community.mycommunity.dto.AccessTokenDTO;
 import huzc.community.mycommunity.dto.GithubUser;
+import huzc.community.mycommunity.mapper.UserMapper;
+import huzc.community.mycommunity.model.User;
 import huzc.community.mycommunity.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -23,6 +27,9 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
 
 
     //指向返回文件
@@ -39,11 +46,19 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if(user != null){
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            //set user values
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             //登录成功，写cookie和session
             //将user对象放入session，框架集成，前端有了银行卡
-            request.getSession().setAttribute("user",user);
+            request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
         }else {
             //登录失败，重新登陆
---
+++ src/main/java/huzc/community/mycommunity/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package huzc.community.mycommunity.mapper;
+
+import huzc.community.mycommunity.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
